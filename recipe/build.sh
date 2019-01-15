f87024233bdb2e32025c8b2aa2b70a1d8c122984
Sterling Smith <smithsp@fusion.gat.com>
2019-01-15 12:51:49 -0800

    Add explicit XML_LIBS

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,8 @@
 ./configure --prefix=$PREFIX --enable-shared --disable-java --disable-dependency-tracking --with-readline=$PREFIX --with-xml-prefix=$PREFIX CFLAGS="-I${PREFIX}/include -I${PREFIX}/include/libxml2 $CFLAGS"
 export CFLAGS="-I${PREFIX}/include -I${PREFIX}/include/libxml2 $CFLAGS"
 export XML_CFLAGS="-I${PREFIX}/include -I${PREFIX}/include/libxml2"
+export XML_LIBS="-L${PREFIX}/lib -lxml2 -lz -llzma -lpthread -liconv -licui18n -licuuc -licudata -lm"
+
 make MOTIF_APS=""
 make install MOTIF_APS=""
 cd mdsobjects/python
