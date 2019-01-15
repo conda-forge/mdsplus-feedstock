2c46b70c3cb8765e0babc9f197c8dcf1d72c1f18
Sterling Smith <smithsp@fusion.gat.com>
2019-01-15 10:36:56 -0800

    Specify XML_CFLAGS manually

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 ./configure --prefix=$PREFIX --enable-shared --disable-java --disable-dependency-tracking --with-readline=$PREFIX --with-xml-prefix=$PREFIX CFLAGS="-I${PREFIX}/include -I${PREFIX}/include/libxml2 $CFLAGS"
 export CFLAGS="-I${PREFIX}/include -I${PREFIX}/include/libxml2 $CFLAGS"
+export XML_CFLAGS="-I${PREFIX}/include -I${PREFIX}/include/libxml2"
 make MOTIF_APS=""
 make install MOTIF_APS=""
 cd mdsobjects/python
