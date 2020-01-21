0216e99f413125e9da0034aea71d3ac3be6e42f1
Sterling Smith <smithsp@fusion.gat.com>
2020-01-21 14:26:01 -0800

    Also build libMdsLib.so

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@ export CFLAGS="-I${PREFIX}/include -I${PREFIX}/include/libxml2 $CFLAGS"
 export XML_CFLAGS="-I${PREFIX}/include -I${PREFIX}/include/libxml2"
 export XML_LIBS="-L${PREFIX}/lib -lxml2 -lz -llzma -lpthread -liconv -licui18n -licuuc -licudata -lm"
 
-make MOTIF_APS="" PARTS="mdsshr mdsdcl treeshr tdishr mdstcpip" MISC_PARTS="tdi include"
-make install MOTIF_APS="" PARTS="mdsshr mdsdcl treeshr tdishr mdstcpip" MISC_PARTS="tdi include"
+make MOTIF_APS="" PARTS="mdsshr mdsdcl treeshr tdishr mdstcpip mdslib" MISC_PARTS="tdi include"
+make install MOTIF_APS="" PARTS="mdsshr mdsdcl treeshr tdishr mdstcpip mdslib" MISC_PARTS="tdi include"
 cd mdsobjects/python
 python setup.py install --single-version-externally-managed --record record.txt
